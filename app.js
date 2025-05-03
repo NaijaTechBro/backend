14b4c7376851494f54bbdf08838ddcc9bfc93630
NaijaTechBro <bakisodiq@gmail.com>
2025-05-03 20:27:18 +0100

    verifyUserRole

--- app.js
+++ app.js
@@ -1,88 +1,3 @@
-// require('dotenv').config();
-
-// const express = require('express');
-
-// // Security
-// const morgan = require('morgan');
-// const cookieParser = require('cookie-parser');
-// const cors = require('cors');
-// const helmet = require('helmet');
-// // const whitelist = require('./config/whitelist')
-
-// const app = express();
-// const path = require('path');
-// const { logger} = require('./middleware/logger');
-// const errorHandler = require('./middleware/errorHandler');
-// const errorMiddleware = require("./middleware/errorMiddleware");
-// const expressSanitizer = require('express-sanitizer');
-// const bodyParser = require('body-parser');
-// const hpp = require('hpp');
-// const connectDB = require('./config/dbConn');
-
-
-
-// // Connecting to Database Environments
-// console.log(process.env.NODE_ENV)
-// connectDB()
-
-// // Middlewares
-// app.use(logger)
-
-// // Cross Origin Resource Sharing
-// app.use(cors({
-//     origin: process.env.GETLISTED_FRONTEND_URL,
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-// }));
-
-// // Error Middleware
-// app.use(errorHandler)
-// app.use(errorMiddleware)
-
-// app.use(express.json({ limit: "30mb", extended: true}))
-
-// app.use(express.urlencoded({ limit: "30mb", extended: false}))
-// app.use(bodyParser.json())
-// // Cookie parser
-// app.use(cookieParser());
-
-// app.use(
-//     helmet.contentSecurityPolicy({
-//         useDefaults: true,
-//         directives: {
-//             "img-src": ["'self'", "https: data:"],
-//         },
-//     }),
-// )
-// app.use(expressSanitizer());
-
-
-// // Dev logging middleware
-// if (process.env.NODE_ENV === 'development') {
-//   app.use(morgan('dev'));
-// }
-
-
-// // Prevent http param pollution
-// app.use(hpp());
-
-// // Mount routers
-// app.use('/api', require('./routes/authRoute'));
-// app.use('/api/verification', require('./routes/roleVerifyRoute'));
-// app.use('/api/startups', require('./routes/startupRoute'));
-// app.use('/api/investors', require('./routes/investorRoute'));
-// app.use('/api/admin', require('./routes/adminRoute'));
-// app.use('/api/startups/connection', require('./routes/connectionRoute'));
-// app.use('/api/startups/views', require('./routes/viewRoute'));
-// app.use('/api/waitlist', require('./routes/waitlistRoute'));
-// app.use('/api/profile', require('./routes/profileRoute'));
-
-
-
-// module.exports = app;
-
-
 require('dotenv').config();
 const express = require('express');
 
@@ -157,7 +72,7 @@ app.use(hpp());
 
 // Mount routers
 app.use('/api', require('./routes/authRoute'));
-app.use('/api/verification', require('./routes/roleVerifyRoute'));
+app.use('/api/verify', require('./routes/roleVerifyRoute'));
 app.use('/api/startups', require('./routes/startupRoute'));
 app.use('/api/investors', require('./routes/investorRoute'));
 app.use('/api/admin', require('./routes/adminRoute'));
