669f9814c92401d076295dbd1f3c224b6e15b4f9
NaijaTechBro <bakisodiq@gmail.com>
2025-05-03 18:23:04 +0100

    updated whitelist

--- app.js
+++ app.js
@@ -1,5 +1,89 @@
-require('dotenv').config();
+// require('dotenv').config();
 
+// const express = require('express');
+
+// // Security
+// const morgan = require('morgan');
+// const cookieParser = require('cookie-parser');
+// const cors = require('cors');
+// const helmet = require('helmet');
+// // const whitelist = require('./config/whitelist')
+
+// const app = express();
+// const path = require('path');
+// const { logger} = require('./middleware/logger');
+// const errorHandler = require('./middleware/errorHandler');
+// const errorMiddleware = require("./middleware/errorMiddleware");
+// const expressSanitizer = require('express-sanitizer');
+// const bodyParser = require('body-parser');
+// const hpp = require('hpp');
+// const connectDB = require('./config/dbConn');
+
+
+
+// // Connecting to Database Environments
+// console.log(process.env.NODE_ENV)
+// connectDB()
+
+// // Middlewares
+// app.use(logger)
+
+// // Cross Origin Resource Sharing
+// app.use(cors({
+//     origin: process.env.GETLISTED_FRONTEND_URL,
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
+
+// // Error Middleware
+// app.use(errorHandler)
+// app.use(errorMiddleware)
+
+// app.use(express.json({ limit: "30mb", extended: true}))
+
+// app.use(express.urlencoded({ limit: "30mb", extended: false}))
+// app.use(bodyParser.json())
+// // Cookie parser
+// app.use(cookieParser());
+
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         useDefaults: true,
+//         directives: {
+//             "img-src": ["'self'", "https: data:"],
+//         },
+//     }),
+// )
+// app.use(expressSanitizer());
+
+
+// // Dev logging middleware
+// if (process.env.NODE_ENV === 'development') {
+//   app.use(morgan('dev'));
+// }
+
+
+// // Prevent http param pollution
+// app.use(hpp());
+
+// // Mount routers
+// app.use('/api', require('./routes/authRoute'));
+// app.use('/api/verification', require('./routes/roleVerifyRoute'));
+// app.use('/api/startups', require('./routes/startupRoute'));
+// app.use('/api/investors', require('./routes/investorRoute'));
+// app.use('/api/admin', require('./routes/adminRoute'));
+// app.use('/api/startups/connection', require('./routes/connectionRoute'));
+// app.use('/api/startups/views', require('./routes/viewRoute'));
+// app.use('/api/waitlist', require('./routes/waitlistRoute'));
+// app.use('/api/profile', require('./routes/profileRoute'));
+
+
+
+// module.exports = app;
+
+
+require('dotenv').config();
 const express = require('express');
 
 // Security
@@ -7,7 +91,7 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const helmet = require('helmet');
-// const whitelist = require('./config/whitelist')
+const { whitelist } = require('./config/whitelist');
 
 const app = express();
 const path = require('path');
@@ -19,8 +103,6 @@ const bodyParser = require('body-parser');
 const hpp = require('hpp');
 const connectDB = require('./config/dbConn');
 
-
-
 // Connecting to Database Environments
 console.log(process.env.NODE_ENV)
 connectDB()
@@ -30,7 +112,16 @@ app.use(logger)
 
 // Cross Origin Resource Sharing
 app.use(cors({
-    origin: process.env.GETLISTED_FRONTEND_URL,
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps, curl requests)
+        if (!origin) return callback(null, true);
+        
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
@@ -41,7 +132,6 @@ app.use(errorHandler)
 app.use(errorMiddleware)
 
 app.use(express.json({ limit: "30mb", extended: true}))
-
 app.use(express.urlencoded({ limit: "30mb", extended: false}))
 app.use(bodyParser.json())
 // Cookie parser
@@ -56,14 +146,12 @@ app.use(
     }),
 )
 app.use(expressSanitizer());
-
-
+ 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
-
+ 
 // Prevent http param pollution
 app.use(hpp());
 
@@ -77,7 +165,5 @@ app.use('/api/startups/connection', require('./routes/connectionRoute'));
 app.use('/api/startups/views', require('./routes/viewRoute'));
 app.use('/api/waitlist', require('./routes/waitlistRoute'));
 app.use('/api/profile', require('./routes/profileRoute'));
-
-
 
 module.exports = app;
--- config/whitelist.js
+++ config/whitelist.js
@@ -1,4 +1,8 @@
-const whitelist = ['http://localhost:5174', 'http://localhost:5173']
+const whitelist = [
+    'http://localhost:5174',
+     'http://localhost:5173',
+     'https://getlisted.site'
+    ]
 
 module.exports = {
     whitelist
